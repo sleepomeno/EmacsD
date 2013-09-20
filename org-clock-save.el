6ef2930297cc7b418e01c1eaff36f9240f687ab7
Gregor Riegler <gregor.riegler@gmail.com>
2013-09-20 17:48:07 +0200

    changes...

--- org-clock-save.el
+++ org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-persist.el - think at <2013-09-13 Fre 18:16>
-(setq stored-clock-history '(("/home/greg/org/uni.org_archive" . 18729) ("/home/greg/org/uni.org_archive" . 19581) ("/home/greg/org/uni.org_archive" . 20143) ("/home/greg/org/uni.org_archive" . 20714) ("/home/greg/org/uni.org_archive" . 20371)))
+;; org-persist.el - think at <2013-09-20 Fre 17:37>
+(setq stored-clock-history '(("/home/greg/org/uni.org_archive" . 19581) ("/home/greg/org/uni.org_archive" . 20143) ("/home/greg/org/uni.org_archive" . 20714) ("/home/greg/org/uni.org_archive" . 20371) ("/home/greg/org/journal.org" . 1769)))
