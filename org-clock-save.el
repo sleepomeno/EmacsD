3def4bae42d02e94483d9ce006bc7c3e1b074178
Gregor Riegler <gregor.riegler@gmail.com>
2013-10-25 15:33:26 +0200

    no idea

--- org-clock-save.el
+++ org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-persist.el - think at <2013-10-09 Mit 21:55>
+;; org-persist.el - think at <2013-10-24 Don 23:21>
 (setq stored-clock-history '(("/home/greg/org/uni.org_archive" . 19581) ("/home/greg/org/uni.org_archive" . 20143) ("/home/greg/org/uni.org_archive" . 20714) ("/home/greg/org/uni.org_archive" . 20371) ("/home/greg/org/journal.org" . 1769)))
