eae3bdd41d1ff4107420c0303cf0da0cb47aa9f4
Gregor Riegler <gregor.riegler@gmail.com>
2014-07-14 00:42:35 +0200

    Add sauron and mu4e configuration in configuration.org

---
+++ email-sauron.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# put the path to your Inbox folder here
+CHECKDIR="/home/$LOGNAME/Maildir/INBOX"
+
+# echo "\ncalled" >> /home/greg/called
+
+sauronmsg () {
+    DBUS_COOKIE="/home/$LOGNAME/.sauron-dbus"
+    if test "x$DBUS_SESSION_BUS_ADDRESS" = "x"; then
+        if test -e $DBUS_COOKIE; then
+                export DBUS_SESSION_BUS_ADDRESS="`cat $DBUS_COOKIE`"
+        fi
+    fi
+    if test -n "x$DBUS_SESSION_BUS_ADDRESS"; then
+        dbus-send --session                          \
+            --dest="org.gnu.Emacs"                   \
+            --type=method_call                       \
+            "/org/gnu/Emacs/Sauron"                  \
+            "org.gnu.Emacs.Sauron.AddMsgEvent"       \
+            string:Mail uint32:3 string:"$1"
+    fi
+}
+
+#
+# -mmin -5: consider only messages that were created / changed in the
+# the last 5 minutes
+#
+for f in `find $CHECKDIR -mmin -5 -a -type f`; do
+				subject=`mu view $f | grep '^Subject:' | sed 's/^Subject://'`
+				from=`mu view $f | grep '^From:' | sed 's/^From://'`
+        sauronmsg "$subject | $from"
+#				echo  "New Mail: $subject | $from" >> /home/greg/called
+done
--- org-clock-save.el
+++ org-clock-save.el
@@ -1,1 +1,2 @@
-;; org-persist.el - think at <2014-06-26 Don 01:13>
+;; org-persist.el - think at <2014-07-12 Sam 01:34>
+(setq stored-clock-history '(("/home/greg/org/home.org" . 2164) ("/home/greg/org/home.org" . 2375) ("/home/greg/org/home.org" . 2574)))
