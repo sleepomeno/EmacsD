162217de33288864806842f5c55dd1259efff4f0
Gregor Riegler <gregor.riegler@gmail.com>
2014-07-27 00:40:15 +0200

    Commt email-sauron.sh logging

--- email-sauron.sh
+++ email-sauron.sh
@@ -3,16 +3,20 @@
 # put the path to your Inbox folder here
 CHECKDIR="/home/$LOGNAME/Maildir/INBOX"
 
-# echo "\ncalled" >> /home/greg/called
+#echo "\ncalled" >> /home/greg/called
 
 sauronmsg () {
     DBUS_COOKIE="/home/$LOGNAME/.sauron-dbus"
     if test "x$DBUS_SESSION_BUS_ADDRESS" = "x"; then
+#				echo "\ntestxdbussessionbusaddressx" >> /home/greg/called
         if test -e $DBUS_COOKIE; then
+#								echo "\testdbuscookie" >> /home/greg/called
                 export DBUS_SESSION_BUS_ADDRESS="`cat $DBUS_COOKIE`"
         fi
     fi
     if test -n "x$DBUS_SESSION_BUS_ADDRESS"; then
+#				echo "\n" >> /home/greg/called
+#				echo $DBUS_SESSION_BUS_ADDRESS >> /home/greg/called
         dbus-send --session                          \
             --dest="org.gnu.Emacs"                   \
             --type=method_call                       \
@@ -30,5 +34,5 @@ for f in `find $CHECKDIR -mmin -5 -a -type f`; do
 				subject=`mu view $f | grep '^Subject:' | sed 's/^Subject://'`
 				from=`mu view $f | grep '^From:' | sed 's/^From://'`
         sauronmsg "$subject | $from"
-#				echo  "New Mail: $subject | $from" >> /home/greg/called
+				echo  "New Mail: $subject | $from" >> /home/greg/called
 done
