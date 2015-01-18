2896c4d2f1a0494591f978e692e496bb5004e162
Gregor Riegler <gregor.riegler@gmail.com>
2015-01-18 02:11:01 +0100

    Small little changes ;)

--- org-clock-save.el
+++ org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-persist.el - think at <2015-01-10 Sam 01:12>
-(setq stored-clock-history '(("/home/greg/org/home.org" . 2392) ("/home/greg/org/home.org" . 2603) ("/home/greg/org/home.org" . 2802)))
+;; org-persist.el - think at <2015-01-18 Son 02:04>
+(setq stored-clock-history '(("/home/greg/org/home.org" . 2468) ("/home/greg/org/home.org" . 2679) ("/home/greg/org/home.org" . 2878)))
